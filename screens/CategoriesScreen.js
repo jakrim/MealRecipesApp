bffe991feeb0222180377c25cdf99ee62730775d
Jesse Krim <jesse.krim@gmail.com>
2019-11-11 15:13:53 -0500

    Add dummy data and render in Categories Screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import { Text, View, Button, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+  StyleSheet
+} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
-  return (
-    <View style={styles.screen}>
-      <Text> The Categories Screen! </Text>
-      <Button
-        title='Go To Meals!'
+  const renderGridItem = itemData => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
         onPress={() => {
-          props.navigation.navigate({ routeName: 'CategoryMeals' });
+          props.navigation.navigate('CategoryMeals', {
+            categoryId: itemData.item.id
+          });
         }}
-      />
-    </View>
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <FlatList
+      keyExtractor={item => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
+};
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white'
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
 
 const styles = StyleSheet.create({
@@ -20,6 +52,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Text, View, Button, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoryMeals = props => {
+  const categoryId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === categoryId);
+
   return (
     <View style={styles.screen}>
       <Text> The Category Meals Screen </Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title='Go to Details'
         onPress={() => {
@@ -20,6 +27,9 @@ const CategoryMeals = props => {
     </View>
   );
 };
+// CategoryMeals.navigationOptions = {
+//   headerTitle: catId
+// }
 
 const styles = StyleSheet.create({
   screen: {
